715ef44bdf665eb5b6ff03cd78cc402dbe1bd6f7
Dan Chalmers <danc@brandwatch.com>
2023-07-24 10:39:02 +0100

    use basic config so that it can be more easily overriden by importing code

--- src/bcr_api/logger.py
+++ src/bcr_api/logger.py
@@ -2,10 +2,10 @@ import logging
 
 
 def get_logger():
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s: %(message)s",
+        datefmt="%H:%M:%S",
+        level=logging.DEBUG,
+    )
     logger = logging.getLogger("bcr_api")
-    logger.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(levelname)s: %(message)s", "%H:%M:%S")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
     return logger
